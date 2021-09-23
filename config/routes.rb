8f6f6372a9e5aefed16a741484974579ae18dd50
jonathan kerbrat <jonathan.kerbrat@hotmail.com>
2021-09-23 23:06:13 +0200

    add update profile

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'projects#index'
-  resources :users, except: %i[destroy show]
+  resources :users, except: %i[new create index destroy show]
   resources :projects
   resources :contacts, only: [:new, :create,:index] do
   end
