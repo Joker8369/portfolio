38d67defd9183b606c3a01521bf9f361e9595ff2
jonathan kerbrat <jonathan.kerbrat@hotmail.com>
2021-09-25 18:08:07 +0200

    js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,14 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "../plugins/letter"
 import { initMapbox } from '../plugins/init_mapbox';
+
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
 
 document.addEventListener('turbolinks:load', () => {
   initMapbox();
